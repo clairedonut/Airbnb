37ecbfdffca4d19d47264d4195428acba45bb0a8
Miss Donut <claire.jacquet@hotmail.com>
2018-02-26 10:18:47 +0100

    added jquery smooth scroll function

---
+++ js/main.js
@@ -0,0 +1,102 @@
+// Soft Scroll
+
+$( function() {
+
+  $('.scroll').click(function(){
+
+    var target = $($(this).attr('href')).offset().top;
+
+    $('html, body').animate({
+      scrollTop: target
+    },'slow');
+  });
+
+});
+
+
+
+// Autocomplete
+var options = {
+
+  url: "js/cities.json",
+
+  getValue: "name",
+
+  list: {
+    match: {
+      enabled: true
+    }
+  },
+
+  theme: "square"
+};
+
+$(function() {
+  $("#cities").easyAutocomplete(options);
+});
+
+// Date picker
+$( function() {
+    var dateFormat = "mm/dd/yy",
+      from = $( "#from" )
+        .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 2
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 2
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
+
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+
+      return date;
+    }
+
+  } );
+
+  // Autocomplete Jquery
+
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  } );
