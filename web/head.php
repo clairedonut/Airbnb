9656d93e59a72a5dc3d8f3219da595c57fc0dc6a
Miss Donut <claire.jacquet@hotmail.com>
2018-02-27 11:17:46 +0100

    re-structured site in php files

---
+++ web/head.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Vacation Rentals, Homes, Experiences & Places - Airbnb</title>
+        <meta name="description" content="Unforgettable trips start with Airbnb. Find adventures nearby or in faraway places and access unique homes, experiences, and places around the world.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" href="favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+        <!-- closest font-family to Airbnb font circular -->
+
+        <!-- styles -->
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+
+
+        <!-- icons stylesheet -->
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+        <!-- jquery plugins stylesheets -->
+        <link rel="stylesheet" href="js/easyautocomplete-1.3.5/easy-autocomplete.themes.css">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
---
+++ web/header.php
@@ -0,0 +1,42 @@
+<body>
+
+  <header>
+
+    <!-- Nav - for mobile and tablet -->
+
+    <div>
+      <div class="brand">
+        <img src="img/icons/logo.png" alt="airbnb logo"/>
+        <img class="arrow" src="img/icons/arrow.png" alt="menu icon"  />
+      </div>
+
+      <nav class="nav display none">
+        <ul class="bulletsnone">
+          <li>
+            <a href="#">Become a Host</a>
+          </li>
+          <li>
+            <a href="#">Help</a>
+          </li>
+          <li>
+            <a href="#">Sign Up</a>
+          </li>
+          <li>
+            <a href="#">Log In</a>
+          </li>
+        </ul>
+      </nav>
+
+    </div>
+
+    <!-- Nav - for desktop -->
+
+    <nav class="desktop">
+      <a href="#">Become a Host</a>
+      <a href="#">Help</a>
+      <a href="#">Sign Up</a>
+      <a href="#">Log In</a>
+    </nav>
+
+
+  </header>
